74bc8351fde6c225d7e0817cc1369fb8d3409871
mohamadalabsi <absimoda1@gmail.com>
2025-03-26 23:52:05 +0100

    first step into question service

---
+++ src/main/java/org/me/learning/questionmicros/QuestionMicroSApplication.java
@@ -0,0 +1,13 @@
+package org.me.learning.questionmicros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionMicroSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionMicroSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/me/learning/questionmicros/QuestionMicroSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.me.learning.questionmicros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionMicroSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
